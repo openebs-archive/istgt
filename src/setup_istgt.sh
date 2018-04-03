d0767bbf4e7b0e972626640ab4ba2fc10c48a6e6
Vishnu Itta <vitta@mayadata.io>
2018-04-03 19:12:34 +0530

    [cstor#130] Setting up travis for istgt repo
    Signed-off-by: Vishnu Itta <vitta@mayadata.io>

--- src/setup_istgt.sh
+++ src/setup_istgt.sh
@@ -7,4 +7,4 @@ sudo cp istgt.conf istgtcontrol.conf /tmp/cstor/
 sudo cp istgt.conf istgtcontrol.conf /usr/local/etc/istgt/
 sudo cp istgt istgtcontrol /usr/local/bin/
 ps -aux | grep "\./istgt" | grep -v grep | sudo kill -9 `awk '{print $2}'`
-sudo ./init.sh volname=vol1 portal=192.168.1.16 path=/tmp/cstor size=10g externalIP=192.168.1.16 replication_factor=3 consistency_factor=2
+sudo ./init.sh volname=vol1 portal=127.0.0.1 path=/tmp/cstor size=10g externalIP=127.0.0.1 replication_factor=3 consistency_factor=2
