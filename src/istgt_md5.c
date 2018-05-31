2c976550d6b3e821437dfc590a8d95ef6574088e
Shubham Bhattar <shubhambhattar@users.noreply.github.com>
2018-05-31 20:48:44 +0530

    `len` will only be checked for `0` as it's of type `size_t`. Please check comments [here](https://github.com/openebs/istgt/pull/27)

--- src/istgt_md5.c
+++ src/istgt_md5.c
@@ -67,7 +67,7 @@ istgt_md5update(ISTGT_MD5CTX *md5ctx, const void *data, size_t len)
 
 	if (md5ctx == NULL)
 		return -1;
-	if (data == NULL || len <= 0)
+	if (data == NULL || len == 0)
 		return 0;
 	rc = MD5_Update(&md5ctx->md5ctx, data, len);
 	return rc;
