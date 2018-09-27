c1d53eb2b414a61cb845fe55fe3e043adc429293
kmova <kiran.mova@openebs.io>
2018-09-27 08:03:35 +0000

    fix(build): fetch the zfs header from the correct branch
    autoreconf is always fetching the zfs header file from zfs repo
    master branch. This make it incompatible to build on released
    branches if there has been a change to the header file on the master.
    
    Added script to determine the zfs branch from which the header
    file should be fetched.
    - If tagged build, fetch from the corresponding tag
    - If master (replication), fetch from the zfs master
    - If non-master, fetch from corresponding zfs branch if it exists
      or use the zfs master
    
    Signed-off-by: kmova <kiran.mova@openebs.io>

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+. ./fetch-zfs-branch.sh
+echo "Using zfs branch - ${ZFS_BUILD_BRANCH}"
+echo $(wget -O /tmp/zrepl_prot.h https://raw.githubusercontent.com/openebs/zfs/${ZFS_BUILD_BRANCH}/include/zrepl_prot.h)
+
 autoreconf -fiv
 rm -Rf autom4te.cache
