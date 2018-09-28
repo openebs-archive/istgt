1719691fc5dfa68927e8956f7806721c7c2e0edf
kmova <kiran.mova@openebs.io>
2018-09-28 10:53:41 +0000

    feat(build): build docker container
    This repository contains the complete code for istgt
    and has a dependency to dynamically fetch a zfs header
    file from openebs/zfs repo.
    
    The previous commit(#81) tried to make the fetch automated
    by using the current branch name as a key to identify the
    dependent branch. However, not all scenarios got covered.
    Besides having the image built in openebs/cstor repo - made
    it more tricky to determine the current branch that needs
    to be used as key.
    
    Also, building in opeenbs/cstor repo kind of de-links the
    commits from the images and depend on a manual trigger
    by the cstor repo to build a docker image.
    
    This PR addresses the following:
    - Add code to build the docker images after a successful build.
    - Put a place holder for specifying the dependent zfs branch
    
    Signed-off-by: kmova <kiran.mova@openebs.io>

---
+++ build_image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+BUILD_DATE=$(date +'%Y%m%d%H%M%S')
+REPO_NAME="openebs/cstor-istgt"
+
+echo "Build image ${REPO_NAME}:ci with BUILD_DATE=${BUILD_DATE}"
+
+cp src/istgt ./docker
+cp src/istgtcontrol ./docker
+cp src/istgt.conf ./docker
+cp src/istgtcontrol.conf ./docker
+
+sudo docker version
+sudo docker build --help
+
+cd docker && \
+ sudo docker build -f Dockerfile -t ${REPO_NAME}:ci --build-arg BUILD_DATE=${BUILD_DATE} . && \
+ IMAGE_REPO=${REPO_NAME} ./push && \
+ cd ..
+
+rm -rf ./docker/istgt*
+
