6a186b1dbb78c8ffeed31ac66754b5240f80ee23
Jayadeep KM <kmjayadeep@gmail.com>
2018-10-06 17:06:35 +0530

    renamed max to max_of to resolve redefine issues
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- src/replication.h
+++ src/replication.h
@@ -33,7 +33,7 @@
  */
 #define	RCMD_MEMPOOL_ENTRIES    (1 << 19)
 
-#define	MAX(a, b) (((a) > (b))?(a):(b))
+#define	MAX_OF(a, b) (((a) > (b))?(a):(b))
 
 typedef enum zvol_cmd_type_e {
 	CMD_IO = 1,
