268559d4c300206470684cabe05bb104c62b1b70
bharathpgp <bharathpgp@gmail.com>
2018-10-06 15:08:31 +0530

    fmt(cstyle): fixes in src/ring_mempool.h

--- src/ring_mempool.h
+++ src/ring_mempool.h
@@ -17,8 +17,10 @@ typedef struct {
 	mempool_reclaim_t *reclaim;
 } rte_smempool_t;
 
-int init_mempool(rte_smempool_t *obj, size_t count, size_t mem_size, size_t offset, const char *mempool_name,
-		mempool_constructor_t *create, mempool_destructor_t *free, mempool_reclaim_t *reclaim, bool initialize);
+int init_mempool(rte_smempool_t *obj, size_t count, size_t mem_size, \
+	size_t offset, const char *mempool_name,	\
+		mempool_constructor_t *create, mempool_destructor_t *free, \
+		mempool_reclaim_t *reclaim, bool initialize);
 int destroy_mempool(rte_smempool_t *obj);
 void * get_from_mempool(rte_smempool_t *obj);
 void put_to_mempool(rte_smempool_t *obj, void *node);
@@ -35,7 +37,7 @@ unsigned get_num_entries_from_mempool(rte_smempool_t *obj);
 
 #ifndef REPLICATION
 #define	REPLICA_LOG
-#define REPLICA_NOTICELOG
+#define	REPLICA_NOTICELOG
 #define	REPLICA_ERRLOG
 #define	REPLICA_WARNLOG
 #endif
