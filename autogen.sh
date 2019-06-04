e0033bf44e55b470468dfd2d6420606422fe288b
Mayank <33252549+mynktl@users.noreply.github.com>
2019-06-04 20:28:58 +0530

    fix(chorus): updating remote path for zrepl_prot.h (#259)
    Signed-off-by: mayank <mayank.patel@mayadata.io>

--- autogen.sh
+++ autogen.sh
@@ -8,7 +8,7 @@ else
 fi
 
 echo "Using zfs branch - ${ZFS_BUILD_BRANCH}"
-echo $(wget -O /tmp/zrepl_prot.h https://raw.githubusercontent.com/openebs/cstor/${ZFS_BUILD_BRANCH}/include/zrepl_prot.h)
+echo $(wget -O /tmp/zrepl_prot.h https://raw.githubusercontent.com/openebs/libcstor/${ZFS_BUILD_BRANCH}/include/zrepl_prot.h)
 
 autoreconf -fiv
 rm -Rf autom4te.cache
