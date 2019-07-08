55cca0f82d441471d0194f09a6980a24bb3268ad
Pawan <pawan@mayadata.io>
2019-07-09 00:51:02 +0530

    fix test cases
    Signed-off-by: Pawan <pawan@mayadata.io>

--- src/istgt_integration_test.c
+++ src/istgt_integration_test.c
@@ -64,8 +64,8 @@ typedef struct snapshot_resp_s {
 	int test_id;
 	int success_cnt;
 	int failure_cnt;
-	int prequired_resp;
-	int psuccess_cnt;
+	int prep_required_resp;
+	int prep_success_cnt;
 } snapshot_resp_t;
 
 snapshot_resp_t snap_resp;
@@ -284,16 +284,15 @@ update_snap_resp_list(spec_t *spec)
 	snap_resp.required_resp = spec->consistency_factor;
 	snap_resp.success_cnt = 0;
 	snap_resp.failure_cnt = 0;
-	snap_resp.psuccess_cnt = 0;
+	snap_resp.prep_success_cnt = 0;
 	snap_resp.test_id = (++snap_resp.test_id) % SNAP_TEST_COUNT;
 	switch (snap_resp.test_id)
 	{
-		case SNAP_CREATE_TIMEOUT:
 		case SNAP_CREATE_FAILURE:
-			snap_resp.prequired_resp = 0;
+			snap_resp.prep_required_resp = 0;
 			break;
 		default:
-			snap_resp.prequired_resp = 1;
+			snap_resp.prep_required_resp = 1;
 	}
 	MTX_UNLOCK(&snap_resp.snap_resp_mtx);
 }
@@ -306,11 +305,11 @@ verify_snap_response(int res)
 		VERIFY(snap_resp.success_cnt < snap_resp.required_resp);
 	} else {
 		VERIFY(snap_resp.success_cnt >= snap_resp.required_resp);
-		VERIFY(snap_resp.psuccess_cnt);
+		VERIFY(snap_resp.prep_success_cnt);
 	}
-	VERIFY(snap_resp.psuccess_cnt == snap_resp.prequired_resp);
+	VERIFY(snap_resp.prep_success_cnt == snap_resp.prep_required_resp);
 	snap_resp.required_resp = 0;
-	snap_resp.prequired_resp = 0;
+	snap_resp.prep_required_resp = 0;
 	MTX_UNLOCK(&snap_resp.snap_resp_mtx);
 }
 
@@ -336,19 +335,13 @@ handle_snap_opcode(rargs_t *rargs, zvol_io_cmd_t *zio_cmd)
 	if (hdr->opcode == ZVOL_OPCODE_SNAP_PREPARE) {
 		MTX_LOCK(&snap_resp.snap_resp_mtx);
 		switch (snap_resp.test_id) {
-			case SNAP_CREATE_TIMEOUT:
-				sleep (10);
-				hdr->status = ZVOL_OP_STATUS_OK;
-				snap_resp.psuccess_cnt++;
-				break;
-
 			case  SNAP_CREATE_FAILURE:
 				hdr->status = ZVOL_OP_STATUS_FAILED;
 				break;
 
 			default:
 				hdr->status = ZVOL_OP_STATUS_OK;
-				snap_resp.psuccess_cnt++;
+				snap_resp.prep_success_cnt++;
 				break;
 		}
 		MTX_UNLOCK(&snap_resp.snap_resp_mtx);
