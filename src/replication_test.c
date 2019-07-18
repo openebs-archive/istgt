1403c52ce9f4a0bb149aba19cf70f3797c6aab98
Pawan Prakash Sharma <pawan@mayadata.io>
2019-07-18 17:45:43 +0530

    fix(travis): execute all test cases in travis (#268)
    Signed-off-by: Pawan <pawan@mayadata.io>

--- src/replication_test.c
+++ src/replication_test.c
@@ -270,13 +270,13 @@ send_mgmt_ack(int fd, zvol_op_code_t opcode, void *buf, char *replica_ip,
 		iovec_count = 1;
 		mgmt_ack_hdr->status = (random() % 2) ? ZVOL_OP_STATUS_FAILED : ZVOL_OP_STATUS_OK;
 		mgmt_ack_hdr->len = 0;
-	} if (opcode == ZVOL_OPCODE_SNAP_PREPARE) {
-		iovec_count = 1;
-		mgmt_ack_hdr->status = (random() % 5 == 0) ? ZVOL_OP_STATUS_FAILED : ZVOL_OP_STATUS_OK;
-		mgmt_ack_hdr->len = 0;
 	} else if (opcode == ZVOL_OPCODE_SNAP_CREATE) {
 		iovec_count = 1;
-		sleep(random()%3 + 1);
+		sleep(random()%2);
+		mgmt_ack_hdr->status = (random() % 5 == 0) ? ZVOL_OP_STATUS_FAILED : ZVOL_OP_STATUS_OK;
+		mgmt_ack_hdr->len = 0;
+	} else if (opcode == ZVOL_OPCODE_SNAP_PREPARE) {
+		iovec_count = 1;
 		mgmt_ack_hdr->status = (random() % 5 == 0) ? ZVOL_OP_STATUS_FAILED : ZVOL_OP_STATUS_OK;
 		mgmt_ack_hdr->len = 0;
 	} else if (opcode == ZVOL_OPCODE_REPLICA_STATUS) {
