f3a7b0455bec55ce7b867c16f4a65abac67988a7
sai chaithanya <sai.chaithanya@mayadata.io>
2020-01-09 21:00:55 +0530

    fix(core): dump cores on persistent location based on env (#298)
    Signed-off-by: mittachaitu <sai.chaithanya@mayadata.io>

--- docker/entrypoint-istgtimage.sh
+++ docker/entrypoint-istgtimage.sh
@@ -5,9 +5,9 @@ trap 'call_exit $LINE_NO' EXIT
 
 call_exit()
 {
-echo "at call_exit.."     
+echo "at call_exit.."
 echo  "exit code:" $?
-echo "reference: "  $0 
+echo "reference: "  $0
 }
 
 if [ ! -f "/usr/local/etc/istgt/istgt.conf" ];then
@@ -19,6 +19,20 @@ touch /usr/local/etc/istgt/auth.conf
 touch /usr/local/etc/istgt/logfile
 export externalIP=0.0.0.0
 service rsyslog start
+
+# Disabling coredumps by default in the shell where istgt runs
+if [ -z "$ENABLE_COREDUMP" ]; then
+	echo "Disabling dumping core"
+	ulimit -c 0
+else
+	echo "Enabling coredumps"
+	ulimit -c unlimited
+	## /var/openebs is mounted as persistent directory on
+	## host machine
+	cd /var/openebs || exit
+	mkdir -p core
+	cd core
+fi
 
 ARCH=$(uname -m)
 export LD_PRELOAD=/usr/lib/${ARCH}-linux-gnu/libjemalloc.so
