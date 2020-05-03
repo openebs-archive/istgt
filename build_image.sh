81fd77a3abdfa4a65bcaf6494bcd2958671bb70a
kmova <kiran.mova@mayadata.io>
2020-05-03 14:19:22 +0000

    refact(build): make the docker images configurable
    Signed-off-by: kmova <kiran.mova@mayadata.io>

--- build_image.sh
+++ build_image.sh
@@ -68,7 +68,7 @@ if [ -z "${DBUILD_SITE_URL}" ]; then
   DBUILD_SITE_URL="https://openebs.io"
 fi
 
-DBUILD_ARGS=--build-arg DBUILD_DATE=${DBUILD_DATE} --build-arg DBUILD_REPO_URL=${DBUILD_REPO_URL} --build-arg DBUILD_SITE_URL=${DBUILD_SITE_URL} --build-arg ARCH=${ARCH}
+DBUILD_ARGS="--build-arg DBUILD_DATE=${DBUILD_DATE} --build-arg DBUILD_REPO_URL=${DBUILD_REPO_URL} --build-arg DBUILD_SITE_URL=${DBUILD_SITE_URL} --build-arg ARCH=${ARCH}"
 
 if [ "${ARCH}" = "x86_64" ]; then
 	REPO_NAME="$IMAGE_ORG/cstor-istgt"
