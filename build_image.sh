b4a2cd143d52550a1f7cd5a13e3f23ab3ce3dcf9
kmova <kiran.mova@mayadata.io>
2020-05-03 12:36:49 +0000

    refact(build): make the docker images configurable
    This commit will help customizing docker images via environment variables:
    - The following ENV help specify custom docker build args
      - DBUILD_SITE_URL ( default: https://openebs.io )
      - DBUILD_REPO_URL ( default: https://github.com/openebs/istgt )
    - The following ENV allows Travis to specify custom image org
      - IMAGE_ORG (default: openebs )
    - The following ENV will allow specifying an extra tag suffix
      - TAG_SUFFIX ( default: none )
    
    Signed-off-by: kmova <kiran.mova@mayadata.io>

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2018-2020 The OpenEBS Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e
 
 pwd
@@ -20,19 +34,54 @@ fi
 make clean
 make
 
-BUILD_DATE=$(date +'%Y%m%d%H%M%S')
+# The images can be pushed to any docker/image registeries
+# like docker hub, quay. The registries are specified in 
+# the `build/push` script.
+#
+# The images of a project or company can then be grouped
+# or hosted under a unique organization key like `openebs`
+#
+# Each component (container) will be pushed to a unique 
+# repository under an organization. 
+# Putting all this together, an unique uri for a given 
+# image comprises of:
+#   <registry url>/<image org>/<image repo>:<image-tag>
+#
+# IMAGE_ORG can be used to customize the organization 
+# under which images should be pushed. 
+# By default the organization name is `openebs`. 
+
+if [ -z "${IMAGE_ORG}" ]; then
+  IMAGE_ORG="openebs"
+fi
+
+# Specify the date of build
+DBUILD_DATE=$(date +'%Y-%m-%dT%H:%M:%SZ')
+
+# Specify the docker arg for repository url
+if [ -z "${DBUILD_REPO_URL}" ]; then
+  DBUILD_REPO_URL="https://github.com/openebs/istgt"
+fi
+
+# Specify the docker arg for website url
+if [ -z "${DBUILD_SITE_URL}" ]; then
+  DBUILD_SITE_URL="https://openebs.io"
+fi
+
+DBUILD_ARGS=--build-arg DBUILD_DATE=${DBUILD_DATE} --build-arg DBUILD_REPO_URL=${DBUILD_REPO_URL} --build-arg DBUILD_SITE_URL=${DBUILD_SITE_URL} --build-arg ARCH=${ARCH}
+
 if [ "${ARCH}" = "x86_64" ]; then
-	REPO_NAME="openebs/cstor-istgt"
+	REPO_NAME="$IMAGE_ORG/cstor-istgt"
 	DOCKERFILE="Dockerfile"
 elif [ "${ARCH}" = "aarch64" ]; then
-	REPO_NAME="openebs/cstor-istgt-arm64"
+	REPO_NAME="$IMAGE_ORG/cstor-istgt-arm64"
 	DOCKERFILE="Dockerfile.arm64"
 else
 	echo "Unusable architecture: ${ARCH}"
 	exit 1
 fi
 
-echo "Build image ${REPO_NAME}:ci with BUILD_DATE=${BUILD_DATE}"
+echo "Build image ${REPO_NAME}:ci with BUILD_DATE=${DBUILD_DATE}"
 
 cp src/istgt ./docker
 cp src/istgtcontrol ./docker
@@ -43,7 +92,7 @@ sudo docker version
 sudo docker build --help
 
 cd docker && \
- sudo docker build -f ${DOCKERFILE} -t ${REPO_NAME}:ci --build-arg BUILD_DATE=${BUILD_DATE} . && \
+ sudo docker build -f ${DOCKERFILE} -t ${REPO_NAME}:ci ${DBUILD_ARGS} . && \
  IMAGE_REPO=${REPO_NAME} ./push && \
  cd ..
 
